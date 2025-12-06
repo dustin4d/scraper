db5ea2e213ba341b2329fef9950c25bbb330525c
Dustin H <DustinHolder14@yahoo.com>
2025-12-06 13:36:32 -0600

    URL Normalizing works

--- crawl.py
+++ crawl.py
@@ -1,3 +1,15 @@
-# stub function, no logic.
-def normalize_url(url)
-    pass
+from urllib.parse import urlparse
+
+def normalize_url(url):
+    print("### DEBUG normalize_url() ###")
+    # remove protocol/scheme
+    print(f"Input URL: {url}")
+    parsed = urlparse(url)
+    print(parsed)
+
+    # concat the netloc and the path
+    normalized = parsed.netloc + parsed.path
+    print(f"NORMALIZED URL: {normalized}")
+    return normalized
+
+    print("### END DEBUG ###")
