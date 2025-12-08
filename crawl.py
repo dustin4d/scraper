496f31eb5d9fb5bca55003247d498a9b0ceb9f32
Dustin H <DustinHolder14@yahoo.com>
2025-12-08 17:25:45 -0600

    Remove print debugging from normalize_url()

--- crawl.py
+++ crawl.py
@@ -2,18 +2,14 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 def normalize_url(url):
-    print("### DEBUG normalize_url() ###")
     # remove protocol/scheme
-    print(f"Input URL: {url}")
     parsed = urlparse(url)
     print(parsed)
 
     # concat the netloc and the path
     normalized = parsed.netloc + parsed.path
-    print(f"NORMALIZED URL: {normalized}")
     return normalized
 
-    print("### END DEBUG ###")
 
 
 # EACH FN NEEDS 3 TEST CASES MINIMUM
