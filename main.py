0ff6a561911b0e24dc65bcefc0545a7983702145
Dustin H <DustinHolder14@yahoo.com>
2025-12-14 20:39:27 -0600

    CLI setup

--- main.py
+++ main.py
@@ -61,7 +61,10 @@ def extract_page_data(html, page_url):
 
 if len(sys.argv) < 2:
     print("no website provided")
+    sys.exit(1)
 elif len(sys.argv) > 2:
     print("too many arguments provided")
+    sys.exit(1)
 elif len(sys.argv) == 2:
     print(f"starting crawl of: {sys.argv[1]}")
+    sys.exit(0)
