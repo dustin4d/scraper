d1b34bbf3e764e124a8529e6036c922b098802d2
Dustin H <DustinHolder14@yahoo.com>
2025-12-14 20:30:28 -0600

    change crawl.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
-def main():
-    print("Hello from web-scraper!")
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import sys
+
+# 3/3 tests written
+def normalize_url(url):
+    # remove protocol/scheme
+    parsed = urlparse(url)
+
+    # concat the netloc and the path
+    normalized = parsed.netloc + parsed.path
+
+    if url == "":
+        return False
+    else:
+        return normalized.lower()
+
+# 2/3 tests written
+def get_h1_from_html(html):
+    # return the text from h1, then make the bs4 object into a str
+    data = BeautifulSoup(html, 'html.parser')
+    h1 = data.h1.string
+    return h1
 
 
-if __name__ == "__main__":
-    main()
+def get_first_paragraph_from_html(html):
+    data = BeautifulSoup(html, "html.parser")
+    actual = data.find("p").get_text()
+    return actual
+
+# return an un-normalized list of all the URLs found within the HTML
+def get_urls_from_html(html, base_url):
+    # TODO: handle relative links -- write a unittest `test_get_urls_from_html_relative`
+    data = BeautifulSoup(html, "html.parser")
+    links = []
+    # use urljoin
+    for link in data.find_all('a'): # find all anchor tags in the soup
+        links.append(urljoin(base_url, link.get('href'))) # append the link's href attr to the list
+    return links # return the list of links in href tags
+
+def get_images_from_html(html, base_url):
+    data = BeautifulSoup(html, "html.parser") # get a bs4 object of the HTML tree
+    links = [] # bs4 returns an array of links
+    for link in data.find_all("img"): # find all the image tags
+        links.append(base_url + link.get("src")) # grab the src attribute (rel path)
+    return links
+
+# 1/3 tests written
+def extract_page_data(html, page_url):
+    soup = BeautifulSoup(html, "html.parser")
+
+    data = {
+        "url": page_url,
+        "h1": get_h1_from_html(html),
+        "first_paragraph": get_first_paragraph_from_html(html),
+        "outgoing_links": get_urls_from_html(html, page_url),
+        "image_urls": get_images_from_html(html, page_url),
+    }
+
+    return data
+
+if len(sys.argv) < 2:
+    print("no website provided")
+elif len(sys.argv) > 2:
+    print("too many arguments provided")
+elif len(sys.argv) == 2:
+    print(f"starting crawl of: {sys.argv[1]}")
