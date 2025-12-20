e28c5f3c4192853434935864c1efc65a3bfbc335
Dustin H <DustinHolder14@yahoo.com>
2025-12-19 22:39:49 -0600

    Isolate execution of CLI arguments so tests can run

--- main.py
+++ main.py
@@ -112,14 +112,13 @@ def crawl_page(base_url, current_url=None, page_data=None):
     # recursively crawl each page returned from get_all_urls()
 
 ### COMMAND LINE STUFF ###
-if len(sys.argv) < 2:
-    print("no website provided")
-    sys.exit(1)
-elif len(sys.argv) > 2:
-    print("too many arguments provided")
-    sys.exit(1)
-elif len(sys.argv) == 2:
-    print(f"starting crawl of: {sys.argv[1]}")
-#    get_html(sys.argv[1])
-    crawl_page(sys.argv[1])
-    sys.exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    else:
+        print(f"starting crawl of: {sys.argv[1]}")
+        crawl_page(sys.argv[1])
